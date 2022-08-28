a0f379a205cfb2332bb66eeb6a2412b173c5d660
Arun Kumar <aprodigalboy@gmail.com>
2022-08-28 10:16:35 +0530

    Removed unnecessary syso

--- backend/src/main/java/com/nura/jewelery/jwt/JwtRequestFilter.java
+++ backend/src/main/java/com/nura/jewelery/jwt/JwtRequestFilter.java
@@ -29,7 +29,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws ServletException, IOException {
-		System.out.println(request.getRequestURL());
 		final String requestTokenHeader = request.getHeader("Authorization");
 
 		String username = null;
